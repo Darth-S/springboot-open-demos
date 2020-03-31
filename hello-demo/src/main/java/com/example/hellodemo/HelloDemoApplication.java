b8a294b4ad7f08f0cb02302d96602be94c357adf
darth-sidious <2791794651@qq.com>
2020-03-31 15:35:18 +0800

    init

--- hello-demo/src/main/java/com/example/hellodemo/HelloDemoApplication.java
+++ hello-demo/src/main/java/com/example/hellodemo/HelloDemoApplication.java
@@ -2,12 +2,20 @@ package com.example.hellodemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
+@Controller
 public class HelloDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloDemoApplication.class, args);
     }
 
+    @RequestMapping(value = "hello",method = RequestMethod.GET)
+    public String sayHello(){
+        return "hello, world";
+    }
 }
