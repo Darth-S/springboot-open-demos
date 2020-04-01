d0271d495f52269bc4fcbbebb5a5f4ff9ee0f0f3
darth-sidious <2791794651@qq.com>
2020-04-01 18:09:02 +0800

    learning spring security------user authentication

--- security-mvc/src/main/java/com/example/security/springmvc/controller/LoginController.java
+++ security-mvc/src/main/java/com/example/security/springmvc/controller/LoginController.java
@@ -4,7 +4,6 @@ import com.example.security.springmvc.model.AuthenticationRequest;
 import com.example.security.springmvc.model.UserDto;
 import com.example.security.springmvc.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
