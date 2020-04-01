58ecb2ab36f5faf2c16a38698322b3da5f207386
darth-sidious <2791794651@qq.com>
2020-04-01 17:01:56 +0800

    learning spring security------basic config

---
+++ security-mvc/src/main/java/com/example/security/springmvc/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.example.security.springmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.example.security.springmvc",
+               includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION,value = Controller.class)})
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+}
