faf247f8ae1e0ca96ba3d31df0d2654ee20b32be
darth-sidious <2791794651@qq.com>
2020-04-09 15:20:44 +0800

    security-boot context

--- security-boot/src/main/java/org/example/security/boot/controller/LoginController.java
+++ security-boot/src/main/java/org/example/security/boot/controller/LoginController.java
@@ -1,5 +1,8 @@
 package org.example.security.boot.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,16 +12,32 @@ public class LoginController {
 
     @RequestMapping(value = "login-success",produces = {"text/plain;charset=UTF-8"})
     public String loginSuccess() {
-        return "登录成功";
+        return getUserName()+"登录成功";
     }
 
     @GetMapping("/r/r1")
     public String r1() {
-        return "访问资源r1";
+        return getUserName()+"访问资源r1";
     }
 
     @GetMapping("/r/r2")
     public String r2() {
-        return "访问资源r2";
+        return getUserName()+"访问资源r2";
+    }
+
+    private String getUserName(){
+        String username = null;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+        if (principal == null) {
+            username = "匿名";
+        }
+        if (principal instanceof UserDetails){
+            UserDetails userDetails = (UserDetails)principal;
+            username = userDetails.getUsername();
+        }else {
+            username = principal.toString();
+        }
+        return username;
     }
 }
